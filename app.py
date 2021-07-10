4ca72eb038ea96c3ea47ac57122bfd234204a4e3
Ubuntu <hossameldin.youssef94@gmail.com>
2021-07-10 06:30:49 +0000

    fix liniting issues

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-return "Hello World, my name is Hossameldin youssef"
+    return "Hello World, my name is Hossameldin youssef"
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
